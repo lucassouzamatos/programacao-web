b0b46e20aee758b71d3477f59a2a22711d2277fa
iLuc1 <lucas.souza69@hotmail.com>
2019-03-08 20:03:10 -0300

    first commit

---
+++ src/main/java/br/unisul/web/PwebApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebApplication.class, args);
+	}
+
+}
